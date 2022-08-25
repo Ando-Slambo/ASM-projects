62c7173bb1502fbcbc592a3968e76f55a8083eab
Ando-Slambo <jadam0303@gmail.com>
2022-08-25 14:58:09 -0400

    I don't care how it looks on gh, it looks good in my editor

--- ASCII_converter/converter.asm
+++ ASCII_converter/converter.asm
@@ -20,7 +20,7 @@ SECTION .bss
 SECTION .text
 global _start
 _start:
-    nop
+    nop					;makes debugger happy
     
     ;setup registers
     mov eax, 139		;put the number to be converted into eax
@@ -29,38 +29,38 @@ _start:
     mov ebx, 10			;put divisor into ebx
     
     converter:
-	xor edx, edx			;zero out edx so it doesn't mess with division
-	div ebx				;eax / ebx -> quotient in eax, remainder in edx
+		xor edx, edx			;zero out edx so it doesn't mess with division
+		div ebx					;eax / ebx -> quotient in eax, remainder in edx
 		
-	;convert to ASCII and push to stack
-	add edx, 30h			;convert remainder to it's ASCII char
-	push edx			;push char onto the stack
-	inc ecx				;inc ecx to keep track of the length of the string (at the end ecx will be one less than the length of the string)
+		;convert to ASCII and push to stack
+		add edx, 30h			;convert remainder to it's ASCII char
+		push edx				;push char onto the stack
+		inc ecx					;inc ecx to keep track of the length of the string (at the end ecx will be one less than the length of the string)
 		
-	cmp eax, 9			;compare eax to 9
-	jg converter			;restart conversion loop if eax is greater than 9 else fall through to append quotient to string
+		cmp eax, 9				;compare eax to 9
+		jg converter			;restart conversion loop if eax is greater than 9 else fall through to append quotient to string
 		
-	;convert to ASCII and append to string
-	add eax, 30h			;convert to ASCII char 
-	mov [ebp], eax			;put char into the beginning of the char array
+		;convert to ASCII and append to string
+		add eax, 30h			;convert to ASCII char 
+		mov [ebp], eax			;put char into the beginning of the char array
 	
 	;pop pushed chars off the stack into our string
 	mov edx, 1			;use edx as our pointer offset (string starts with 1 char in it so we must start with 1 in edx) 	
 	string_constructor:
-		pop eax				;pop char to eax
+		pop eax					;pop char to eax
 		mov [ebp+edx], eax		;mov char to pointer+offset
-		inc edx				;inc edx to write to the next byte next iteration
-		loop string_constructor		;will repeat loop until ecx is 0 then fall through to print
-		mov byte [ebp+edx], 10		;append \n to string
-		inc edx				;inc edx to keep track of length of string
+		inc edx					;inc edx to write to the next byte next iteration
+		loop string_constructor	;will repeat loop until ecx is 0 then fall through to print
+		mov byte [ebp+edx], 10	;append \n to string
+		inc edx					;inc edx to keep track of length of string
 		
 	;print
 	mov eax, 4			;sys_write
 	mov ebx, 1			;stdout
-	mov ecx, ebp			;put pointer to string in ecx
+	mov ecx, ebp		;put pointer to string in ecx
 	int 80h				;system interrupt to call service dispatcher
 
     ;terminate program
     mov eax, 1			;sys_exit
     mov ebx, 0			;return value
-    int 80h			;system interrupt to call service dispatcher
+    int 80h				;system interrupt to call service dispatcher
